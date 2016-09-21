a3cfc949bc404f40ad5bef82290282cc083c5700
tirys <joanjezzgames@gmail.com>
2016-09-21 16:15:00 -0300

    Smarty funcionando [Controller->View]

--- controller/index.php
+++ controller/index.php
@@ -1,2 +1,13 @@
 <?php
-	echo 'Aqui vai ser o arquivo que vai montar as informações (criar regras de negócios) chamando o arquivo dentro da pasta VIEW com smarty';
+	//echo 'Aqui vai ser o arquivo que vai montar as informações (criar regras de negócios) chamando o arquivo dentro da pasta VIEW com smarty';
+    require '../lib/smarty/libs/Smarty.class.php';
+
+    $smarty = new Smarty;
+
+    //$smarty->force_compile = true;
+    $smarty->debugging = true;
+    $smarty->caching = true;
+    $smarty->cache_lifetime = 120;
+
+    $smarty->assign("nome", "Teste Jéssica", true);
+    $smarty->display('../view/index.tpl');
