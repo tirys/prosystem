98d87b4d48c00041793a633885275e5a58ecf426
tirys <joanjezzgames@gmail.com>
2016-09-21 15:21:30 -0300

    Estrutura Inicial (com .htaccess)

---
+++ controller/index.php
@@ -0,0 +1,2 @@
+<?php
+	echo 'Aqui vai ser o arquivo que vai montar as informações (criar regras de negócios) chamando o arquivo dentro da pasta VIEW com smarty';
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+	echo 'Não há nada aqui :)';
