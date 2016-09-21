15e9f2e2e1768cc0676eb7130f719d740b023680
tirys <joanjezzgames@gmail.com>
2016-09-21 17:16:44 -0300

    Adicionado página de login (não programada) e dependencias do tema a ser utilizado

--- pages/index.php
+++ pages/index.php
@@ -3,6 +3,7 @@
 //Dependencias
 require ('../lib/smarty/libs/Smarty.class.php');
 require('../controller/index.php');
+include('../config.php');
 
 //Smarty configs
 $smarty = new Smarty;
@@ -15,4 +16,5 @@ $jessica->setNome('Jéssica Teste');
 
 //Setando objeto no template
 $smarty->assign("nome", $jessica->getNome(), true);
+$smarty->assign("url",URL_INSTALACAO);
 $smarty->display('../view/index.tpl');
---
+++ pages/templates_c/48dc54fb59559dbf7559ea31173da0265d05a627_0.file.index.tpl.cache.php
@@ -0,0 +1,207 @@
+<?php
+/* Smarty version 3.1.30, created on 2016-09-21 22:12:31
+  from "C:\wamp\www\prosystem\prosystem\view\index.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_57e2e9af9a9727_06456468',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '48dc54fb59559dbf7559ea31173da0265d05a627' => 
+    array (
+      0 => 'C:\\wamp\\www\\prosystem\\prosystem\\view\\index.tpl',
+      1 => 1474488741,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_57e2e9af9a9727_06456468 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '1405557e2e9af967096_42574484';
+?>
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="utf-8" />
+    <title>Prospecta - Login</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="Preview page of Metronic Admin Theme #1 for " name="description" />
+    <meta content="" name="author" />
+    <!-- Estilos principais -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=all" rel="stylesheet" type="text/css" />
+    <link href="view/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="view/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="view/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="view/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Plugins da Pagina -->
+    <link href="view/assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+    <link href="view/assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Estilo global do tema -->
+    <link href="view/assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="view/assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+
+
+    <link href="view/assets/pages/css/login-5.min.css" rel="stylesheet" type="text/css" />
+
+    <link rel="shortcut icon" href="favicon.ico" /> </head>
+<!-- END HEAD -->
+
+<body class=" login">
+<!-- BEGIN : LOGIN PAGE 5-1 -->
+<div class="user-login-5">
+    <div class="row bs-reset">
+        <div class="col-md-6 bs-reset mt-login-5-bsfix">
+            <div class="login-bg" style="background-image:url(view/assets/pages/img/login/bg1.jpg)">
+                <img class="login-logo" src="view/assets/pages/img/login/logo.png" /> </div>
+        </div>
+        <div class="col-md-6 login-container bs-reset mt-login-5-bsfix">
+            <div class="login-content">
+                <h1>Metronic Admin Login</h1>
+                <p> Lorem ipsum dolor sit amet, coectetuer adipiscing elit sed diam nonummy et nibh euismod aliquam erat volutpat. Lorem ipsum dolor sit amet, coectetuer adipiscing. </p>
+                <form action="javascript:;" class="login-form" method="post">
+                    <div class="alert alert-danger display-hide">
+                        <button class="close" data-close="alert"></button>
+                        <span>Enter any username and password. </span>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <input class="form-control form-control-solid placeholder-no-fix form-group" type="text" autocomplete="off" placeholder="Username" name="username" required/> </div>
+                        <div class="col-xs-6">
+                            <input class="form-control form-control-solid placeholder-no-fix form-group" type="password" autocomplete="off" placeholder="Password" name="password" required/> </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="rem-password">
+                                <label class="rememberme mt-checkbox mt-checkbox-outline">
+                                    <input type="checkbox" name="remember" value="1" /> Remember me
+                                    <span></span>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-sm-8 text-right">
+                            <div class="forgot-password">
+                                <a href="javascript:;" id="forget-password" class="forget-password">Forgot Password?</a>
+                            </div>
+                            <button class="btn green" type="submit">Sign In</button>
+                        </div>
+                    </div>
+                </form>
+                <!-- BEGIN FORGOT PASSWORD FORM -->
+                <form class="forget-form" action="javascript:;" method="post">
+                    <h3 class="font-green">Forgot Password ?</h3>
+                    <p> Enter your e-mail address below to reset your password. </p>
+                    <div class="form-group">
+                        <input class="form-control placeholder-no-fix form-group" type="text" autocomplete="off" placeholder="Email" name="email" /> </div>
+                    <div class="form-actions">
+                        <button type="button" id="back-btn" class="btn green btn-outline">Back</button>
+                        <button type="submit" class="btn btn-success uppercase pull-right">Submit</button>
+                    </div>
+                </form>
+                <!-- END FORGOT PASSWORD FORM -->
+            </div>
+            <div class="login-footer">
+                <div class="row bs-reset">
+                    <div class="col-xs-5 bs-reset">
+                        <ul class="login-social">
+                            <li>
+                                <a href="javascript:;">
+                                    <i class="icon-social-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">
+                                    <i class="icon-social-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">
+                                    <i class="icon-social-dribbble"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-7 bs-reset">
+                        <div class="login-copyright text-right">
+                            <p>Copyright &copy; Keenthemes 2015</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END : LOGIN PAGE 5-1 -->
+<!--[if lt IE 9]>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/respond.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/excanvas.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/ie8.fix.min.js"><?php echo '</script'; ?>
+>
+<![endif]-->
+<!-- BEGIN CORE PLUGINS -->
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/jquery.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/js.cookie.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/select2/js/select2.full.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="view/assets/global/plugins/backstretch/jquery.backstretch.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<?php echo '<script'; ?>
+ src="view/assets/global/scripts/app.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<!-- END THEME GLOBAL SCRIPTS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<?php echo '<script'; ?>
+ src="view/assets/pages/scripts/login-5.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+<!-- END PAGE LEVEL SCRIPTS -->
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<!-- END THEME LAYOUT SCRIPTS -->
+<!-- Google Code for Universal Analytics -->
+<?php }
+}
