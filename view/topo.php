3a8600b250b02bb8e23203cc4508cb33d23799ec
tirys <joanjezzgames@gmail.com>
2016-09-22 17:31:46 -0300

    Corrigido problema do fontawesome

--- view/topo.php
+++ view/topo.php
@@ -31,7 +31,7 @@ if(count($cookie)>0) {
     <meta content="" name="author" />
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=all" rel="stylesheet" type="text/css" />
-    <link href="view/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="view/libs/fontawesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <link href="view/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
     <link href="view/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="view/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
