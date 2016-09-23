c571d0e737bd655b330f4d540c5ac9faf593a584
tirys <joanjezzgames@gmail.com>
2016-09-23 09:34:52 -0300

    Adicionado um arquivo com um esqueleto de página para usar como padrão.

---
+++ view/rodape.php
@@ -0,0 +1,107 @@
+<!-- BEGIN FOOTER -->
+<div class="page-footer">
+    <div class="page-footer-inner">  Agência Prospecta &copy;
+        &nbsp;|&nbsp;
+        2016
+    </div>
+    <div class="scroll-to-top">
+        <i class="icon-arrow-up"></i>
+    </div>
+</div>
+<!-- END FOOTER -->
+</div>
+
+<!--[if lt IE 9]>
+<script src="view/assets/global/plugins/respond.min.js"></script>
+<script src="view/assets/global/plugins/excanvas.min.js"></script>
+<script src="view/assets/global/plugins/ie8.fix.min.js"></script>
+<![endif]-->
+<!-- BEGIN CORE PLUGINS -->
+<script src="view/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="view/assets/global/plugins/moment.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/morris/morris.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/morris/raphael-min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/counterup/jquery.waypoints.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/counterup/jquery.counterup.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/amcharts/amcharts.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/amcharts/serial.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/amcharts/pie.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/amcharts/radar.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/amcharts/themes/light.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/amcharts/themes/patterns.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/amcharts/themes/chalk.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/ammap/ammap.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/ammap/maps/js/worldLow.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/amcharts/amstockcharts/amstock.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/horizontal-timeline/horizontal-timeline.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="view/assets/global/scripts/app.min.js" type="text/javascript"></script>
+<!-- END THEME GLOBAL SCRIPTS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="view/assets/pages/scripts/dashboard.min.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="view/assets/layouts/layout/scripts/layout.min.js" type="text/javascript"></script>
+<script src="view/assets/layouts/layout/scripts/demo.min.js" type="text/javascript"></script>
+<script src="view/assets/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+<script src="view/assets/layouts/global/scripts/quick-nav.min.js" type="text/javascript"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
+<!-- Google Code for Universal Analytics -->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','../../../../../www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-37564768-1', 'auto');
+    ga('send', 'pageview');
+</script>
+<!-- End -->
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="http://www.googletagmanager.com/ns.html?id=GTM-W276BJ"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '../../../../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W276BJ');</script>
+<!-- End -->
+
+<!-- Google Code for Remarketing Tag -->
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 984933920;
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="../../../../../www.googleadservices.com/pagead/f.txt"></script>
+<!-- End -->
+</body>
+
+
+
+<!-- Mirrored from keenthemes.com/preview/metronic/theme/admin_1/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 14 Sep 2016 12:33:22 GMT -->
+</html>
