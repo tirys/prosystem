8a33cbc48810b90d5814b53b6ca9775242794712
tirys <joanjezzgames@gmail.com>
2016-09-23 09:18:22 -0300

    Adicionada página de erro 404

---
+++ erros/404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+    <!--<![endif]-->
+    <!-- BEGIN HEAD -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+        <meta charset="utf-8" />
+        <title>Prospecta | 404</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="Preview page of Metronic Admin Theme #1 for 404 page option 3" name="description" />
+        <meta content="" name="author" />
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=all" rel="stylesheet" type="text/css" />
+        <link href="../view/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="../view/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="../view/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="../view/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+        <link href="../view/assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+        <link href="../view/assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+        <link href="../view/assets/pages/css/error.min.css" rel="stylesheet" type="text/css" />
+        <link rel="shortcut icon" href="favicon.ico" /> </head>
+
+    <body class=" page-404-3">
+        <div class="page-inner">
+            <img src="../view/assets/pages/media/pages/earth.jpg" class="img-responsive" alt=""> </div>
+        <div class="container error-404">
+            <h1>404</h1>
+            <h2>Houston, nós temos um problema..</h2>
+            <p> Atualmente, a página que você está tentando acessar não existe.</p>
+            <p>
+                <a href="../dashboard" class="btn red btn-outline"> Retornar para a home </a>
+                <br> </p>
+        </div>
+
+<!--[if lt IE 9]>
+<script src="../view/assets/global/plugins/respond.min.js"></script>
+<script src="../view/assets/global/plugins/excanvas.min.js"></script>
+<script src="../view/assets/global/plugins/ie8.fix.min.js"></script>
+<![endif]-->
+
+        <script src="../view/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+        <script src="../view/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="../view/assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+        <script src="../view/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+        <script src="../view/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+        <script src="../view/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+        <script src="../view/assets/global/scripts/app.min.js" type="text/javascript"></script>
+
+<script type="text/javascript" src="../../../../../www.googleadservices.com/pagead/f.txt"></script>
+</body>
+</html>
