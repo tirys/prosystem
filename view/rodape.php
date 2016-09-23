ad94ebe53019cb6307526ce9dfd4b0d2a9a8b375
tirys <joanjezzgames@gmail.com>
2016-09-23 10:37:29 -0300

    Voltando bkp rodape

--- view/rodape.php
+++ view/rodape.php
@@ -19,6 +19,10 @@
 <!-- BEGIN CORE PLUGINS -->
 <script src="view/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
 <script src="view/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="view/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
 <!-- END CORE PLUGINS -->
 
 <!-- BEGIN PAGE LEVEL PLUGINS -->
