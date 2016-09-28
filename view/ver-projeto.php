f6903132b047f0b75c5196f88c993358bcfa2791
tirys <joanjezzgames@gmail.com>
2016-09-28 17:51:23 -0300

    Listando envolvidos no projeto ( falta só listar a funcao do usuario)

--- view/ver-projeto.php
+++ view/ver-projeto.php
@@ -18,6 +18,8 @@ $tarefas = $conexao::fetch("SELECT tt.*, te.tb_empresas_nome FROM tb_tarefas tt,
 //Realizar os cálculos de status
 
 //Obter os participantes do projeto através das tarefas
+$usuarios = $conexao::fetch("SELECT tu.* FROM tb_usuarios tu, tb_tarefas tt WHERE tt.tb_tarefas_projeto = {$id} AND tt.tb_tarefas_funcionario = tu.id");
+
 ?>
 <!-- BEGIN PAGE LEVEL STYLES -->
 <link href="view/assets/pages/css/about.min.css" rel="stylesheet" type="text/css" />
@@ -246,55 +248,20 @@ $tarefas = $conexao::fetch("SELECT tt.*, te.tb_empresas_nome FROM tb_tarefas tt,
                     </div>
                 </div>
                 <div class="row margin-bottom-20 stories-cont">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="portlet light">
-                            <div class="photo">
-                                <img src="view/assets/pages/media/users/teambg1.jpg" alt="" class="img-responsive" /> </div>
-                            <div class="title">
-                                <span> Mark Wahlberg </span>
-                            </div>
-                            <div class="desc">
-                                <span> We are at our very best, and we are happiest, when we are fully engaged in work we enjoy on the journey toward the goal we've established for ourselves. </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="portlet light">
-                            <div class="photo">
-                                <img src="view/assets/pages/media/users/teambg2.jpg" alt="" class="img-responsive" /> </div>
-                            <div class="title">
-                                <span> Lindsay Lohan </span>
-                            </div>
-                            <div class="desc">
-                                <span> Do what you love to do and give it your very best. Whether it's business or baseball, or the theater, or any field. If you don't love what you're doing and you can't give it your best, get out of it. </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="portlet light">
-                            <div class="photo">
-                                <img src="view/assets/pages/media/users/teambg5.jpg" alt="" class="img-responsive" /> </div>
-                            <div class="title">
-                                <span> John Travolta </span>
-                            </div>
-                            <div class="desc">
-                                <span> To be nobody but yourself in a world which is doing its best, to make you everybody else means to fight the hardest battle which any human being can fight; and never stop fighting. </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="portlet light">
-                            <div class="photo">
-                                <img src="view/assets/pages/media/users/teambg8.jpg" alt="" class="img-responsive" /> </div>
-                            <div class="title">
-                                <span> Tom Brady </span>
-                            </div>
-                            <div class="desc">
-                                <span> You have to accept whatever comes and the only important thing is that you meet it with courage and with the best that you have to give. Never give up, never surrender. Go all out or gain nothing. </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+                    <?php
+                        foreach ($usuarios as $usuario) {
+                            echo '<div class="col-lg-3 col-md-6"><div class="portlet light"><div class="photo">';
+                            echo "<img src='view/images/{$usuario['tb_usuarios_foto']}' alt='' class='img-responsive'/></div>";
+                            echo '<div class="title">';
+                            echo "<span>{$usuario['tb_usuarios_nome']}</span>";
+                            echo '<div class="desc">';
+                            echo '<span>Colocar a funcao aqui</span>';
+                            echo '</div>';
+                            echo '</div></div></div>';
+                        }
+                    ?>
+</div>
                 <!--END PARTICIPANTES-->
 
             </div>
