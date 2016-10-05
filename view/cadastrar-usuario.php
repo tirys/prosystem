be1640d352eea21f9b4d162c3968ef76bddce323
tirys <joanjezzgames@gmail.com>
2016-10-05 17:20:51 -0300

    Bugs JS arrumados

--- view/cadastrar-usuario.php
+++ view/cadastrar-usuario.php
@@ -253,12 +253,12 @@ if($idUser != ""){
 
     <!-- BEGIN CORE PLUGINS -->
     <script src="view/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-    <script src="view/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="view/assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
-    <script src="view/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-    <script src="view/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-    <script src="view/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js"
-            type="text/javascript"></script>
+
+<!--    <script src="view/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>-->
+<!--    <script src="view/assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>-->
+<!--    <script src="view/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>-->
+<!--    <script src="view/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>-->
+<!--    <script src="view/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>-->
     <!-- END CORE PLUGINS -->
     <!-- BEGIN PAGE LEVEL PLUGINS -->
     <script src="view/assets/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
