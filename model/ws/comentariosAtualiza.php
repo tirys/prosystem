fe11869cc6f99e01151c347f887d9dedf94c9694
tirys <joanjezzgames@gmail.com>
2016-10-24 11:47:19 -0200

    Ajustado horário exibido nos comentários. Se foi hoje mostra apenas horário, senão, a data em que foi inserido.

--- model/ws/comentariosAtualiza.php
+++ model/ws/comentariosAtualiza.php
@@ -37,7 +37,23 @@ if($acao=='listar') {
         $html .= '<div class="message">';
         $html .= '<span class="arrow"> </span>';
         $html .= '<a href="javascript:;" class="name">'.$comentario['tb_usuarios_nome'].'</a>';
-        $html .= '<span class="datetime"> at 20:11 </span>';
+
+        $hoje = date("Y-m-d"); //pegando o dia de hoje
+        $diaComentario = explode(" ",$comentario['tb_comentarios_data']); //separando dia da hora
+
+        if($diaComentario[0]==$hoje) { //se o comentario foi hoje
+            $diaComentario = explode(":",$diaComentario[1]); //cortar em horas, minutos e segundos
+
+            $horario = "<span style='color:#9c9c9c'>hoje às " .$diaComentario[0].":".$diaComentario[1]."</span>"; //mostrar só a hora e minutos
+        }
+        else {
+            $diaComentario = explode("-",$diaComentario[0]);
+
+            $horario = "<span style='color:#9c9c9c'>em ".$diaComentario[2] . "/" . $diaComentario[1] . "/" . $diaComentario[0]."</span>";
+
+        }
+
+        $html .= '<span class="datetime"> '.$horario.'</span>';
         $html .= '<span class="body">';
 
         $html .= $comentario['tb_comentarios_texto'];
