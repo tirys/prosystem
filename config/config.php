9868f9c5cfe08bcb62483d926dc94e674fb54e49
tirys <joanjezzgames@gmail.com>
2016-10-25 11:44:57 -0200

    Mudança de cadastro de aprovação de tarefas

--- config/config.php
+++ config/config.php
@@ -4,12 +4,6 @@ define('URL_INSTALACAO', 'localhost/prosystem/prosystem/');
 define('BASE_URL','/prosystem/prosystem/');
 //Configuração de banco de dados
 
-//LOCALHOST
-//define('HOST_DB','localhost');
-//define('NAME_DB','sistemaprospecta');
-//define('USER_DB','root');
-//define('PASS_DB','');
-
 //HOST EXTERNO
 define('HOST_DB','mysql.agenciaprospecta.com.br');
 define('NAME_DB','agenciaprospec52');
