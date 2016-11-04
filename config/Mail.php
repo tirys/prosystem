8849bd946e9a2d2211cb62fdab60236d5f75adc8
tirys <joanjezzgames@gmail.com>
2016-11-04 17:21:26 -0200

    Incluindo classe de envio de mail

---
+++ config/Mail.php
@@ -0,0 +1,39 @@
+<?php
+    use PHPMailer;
+//    include ('../libs/phpmailer/phpmailer/class.phpmailer.php');
+//    include ('../libs/phpmailer/phpmailer/class.smtp.php');
+
+
+ class Email {
+
+    public function enviarEmailAtibuido($quemAtribuido, $quemAtribuiu, $tarefaId) {
+        $email = new PHPMailer();
+        $email = $this->criarEstrutura();
+
+        $email->addAddress($quemAtribuido, "Jéssica");
+        $email->Subject = "[".utf8_decode(' PROSPECTA ')."] - Nome Tarefa";
+
+        $mensagem = "Você foi atribuído como responsável pela tarefa: <a href='www.agenciaprospecta.com.br'> nometarefacomlinnk </a>";
+
+        $email->msgHTML(nl2br(utf8_decode($mensagem)));
+        $email->send();
+    }
+
+    public function criarEstrutura() {
+        $email = new PHPMailer();
+        $email->isSMTP();
+        $email->SMTPDebug  = 0;
+        $email->setLanguage("br","proton/tools/phpmailer_v51/language/");
+        $email->SMTPAuth   = true;
+        $email->SMTPAutoTLS = false;
+        $email->Host       = "smtp.agenciaprospecta.com.br";
+        $email->Port       = 587;
+        $email->Username   = "send@agenciaprospecta.com.br";
+        $email->Password   = "prospecta14";
+        $email->addReplyTo($email,'Reply');
+        $email->setFrom('send@agenciaprospecta.com.br', 'web');
+
+        return $email;
+    }
+
+}
