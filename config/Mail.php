6f09835cfc6814eda435c8eee91a205244fe9490
tirys <joanjezzgames@gmail.com>
2016-11-04 17:35:12 -0200

    [FUNCIONANDO] Incluindo classe de envio de mail

--- config/Mail.php
+++ config/Mail.php
@@ -1,18 +1,31 @@
 <?php
     include ('../libs/phpmailer/phpmailer/class.phpmailer.php');
     include ('../libs/phpmailer/phpmailer/class.smtp.php');
+    include ('pacoteClasses.php');
 
 
  class Email {
 
     public function enviarEmailAtibuido($quemAtribuido, $quemAtribuiu, $tarefaId) {
+        $conexao = new classeConexao();
         $email = new PHPMailer();
         $email = $this->criarEstrutura();
 
-        $email->addAddress("jessica@agenciaprospecta.com.br", "Jéssica");
-        $email->Subject = "[".utf8_decode(' PROSPECTA ')."] - Nome Tarefa";
 
-        $mensagem = "Você foi atribuído como responsável pela tarefa: <a href='www.agenciaprospecta.com.br'> nometarefacomlinnk </a>";
+        //receber id quem foi atribuido
+            //pegar os dados de quem foi atribuido
+            //$atribuido = $conexao::fetchuniq("SELECT * FROM tb_usuarios WHERE id = {$quemAtribuido}");
+        //receber id quem atribuiu
+            //pegar os dados de quem atribuiu
+            $atribuiu = $conexao::fetchuniq("SELECT * FROM tb_usuarios WHERE id = {$quemAtribuiu}");
+        //receber id tarefa que foi atualizada
+            //pegar os dados da tarefa que foi atualizada
+            $tarefa = $conexao::fetchuniq("SELECT * FROM tb_tarefas WHERE id = {$tarefaId}");
+
+        $email->addAddress("jessica@agenciaprospecta.com.br", "Jéssica");
+        $email->Subject = "[".utf8_decode(' PROSPECTA ')."] - {$tarefa['tb_tarefas_nome']}";
+
+        $mensagem = "O usuário {$atribuiu['tb_usuarios_nome']} atribuiu você como responsável pela tarefa: <a href='www.agenciaprospecta.com.br'> nometarefacomlinnk </a>";
 
         $email->msgHTML(nl2br(utf8_decode($mensagem)));
         $email->send();
