1222fd13b84bf5b2cf6e397fd78b70b7e0f37a8d
tirys <joanjezzgames@gmail.com>
2016-11-04 16:56:54 -0200

    Inclusão de TODO

--- view/topo.php
+++ view/topo.php
@@ -37,7 +37,7 @@ if(count($cookie)>0) {
 <head>
     <base href="<?=BASE_URL?>"/>
     <meta charset="utf-8" />
-    <title>Prospecta - Dashboard</title>
+    <title>Prospecta - Sistema</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport" />
     <meta content="Preview page of Metronic Admin Theme #1 for statistics, charts, recent events and reports" name="description" />
